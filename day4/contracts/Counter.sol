c9a788432b043317ffcfb8f2679705b99bdd70cd
asli <isinasli@gmail.com>
2025-09-30 17:56:37 +0300

    Add onlyOwner modifier and add functions and tests for that

--- day4/contracts/Counter.sol
+++ day4/contracts/Counter.sol
@@ -3,9 +3,21 @@ pragma solidity ^0.8.28;
 
 contract Counter {
   uint256 public count;
+  address public owner;
 
   constructor(uint256 _initialCount) {
     count = _initialCount;
+    owner = msg.sender;
+  }
+  
+  // Modifier to check that the caller is the owner of
+  // the contract.
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Sorry, not the owner");
+    // Underscore is a special character only used inside
+    // a function modifier and it tells Solidity to
+    // execute the rest of the code.
+    _;
   }
 
   // Function to get the current count
@@ -18,8 +30,13 @@ contract Counter {
     count += 1;
   }
 
+  // Function to increment count by 10
+  function superInc() public onlyOwner{
+    count += 10;
+  }
+
   // Function to decrement count by 1
-  function dec() public {
+  function dec() public onlyOwner {
     require(count > 0, "You can't decrement below 0");
     count -= 1;
   }
--- day4/test/Counter.ts
+++ day4/test/Counter.ts
@@ -5,6 +5,9 @@ const { ethers } = await network.connect();
 
 describe("Counter", function () {
   it("Should increment by 1 when calling the inc() function", async function () {
+    const [signer0] = await ethers.getSigners();
+    console.log("deploying contract as", signer0.address);
+
     // Constructor arguments are passed as an array in the second parameter
     const counter = await ethers.deployContract("Counter", [10]);
     expect(await counter.count()).to.equal(10);
@@ -12,5 +15,18 @@ describe("Counter", function () {
     const incTx = await counter.inc();
     await incTx.wait();
     expect(await counter.count()).to.equal(11);
+
+    expect(await counter.owner()).to.equal(signer0.address);
+  });
+
+  it("Should revert when a non owner account calling the dec() function", async function () {
+    const [signer0, signer1] = await ethers.getSigners();
+    console.log("deploying contract as", signer0.address);
+
+    // Constructor arguments are passed as an array in the second parameter
+    const counter = await ethers.deployContract("Counter", [10]);
+
+    // Send transaction with specified account otherwise it is always signer0
+    await expect(counter.connect(signer1).dec()).to.be.revertedWith("Sorry, not the owner");
   });
 });
